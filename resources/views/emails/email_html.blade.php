261f0c79aad1fd85336c712e0b0bd3681442403c
gumiimac3 <>
2020-02-28 17:04:31 +0700

    commit dau tien

---
+++ resources/views/emails/email_html.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Vidiz!! em yeu anh! Nhung anh Will cu lam phien em hoai dut khua cho em di!!!!
+    <img src="https://a7ddaef8.ngrok.io/contact/image.jpg?email=vidizzbidizz" />
+</body>
+</html>
